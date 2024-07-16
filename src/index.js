f75e2b98c3fa42afeeef9b9c0bcc921ba5cd8cf0
Oliver <hoangminhchaucp54@gmail.com>
2024-07-16 22:53:12 +0700

    Add redux

--- src/index.js
+++ src/index.js
@@ -17,6 +17,8 @@ import React from "react";
 import * as ReactDOMClient from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "App";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 const container = document.getElementById("root");
 
@@ -25,6 +27,8 @@ const root = ReactDOMClient.createRoot(container);
 
 root.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>
 );
